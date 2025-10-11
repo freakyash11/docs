5461e96dd4ef4a22b8288f53d64436bd1c1d3255
freakyash11 <122809436+freakyash11@users.noreply.github.com>
2025-10-11 14:34:34 +0530

    updated import

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from '@clerk/react';
+import { useAuth } from '@clerk/clerk-react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Dashboard() {
