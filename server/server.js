e5c80ba7ab444f608d5289f741c12a23f3c1bc11
freakyash11 <122809436+freakyash11@users.noreply.github.com>
2025-10-25 14:59:45 +0530

    update

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.some(allowed => typeof allowed === 'string' ? allowed === origin : allowed.test(origin))) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
 
